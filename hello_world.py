6aa1aad6264f5d6bffbdca19028d568824e64dab
Roope Mantere <roope.mantere@tuni.fi>
2022-12-22 01:46:26 +0200

    fix

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("Hello world!")
-<<<<<<< HEAD
-=======
-print("Hello world!")
->>>>>>> b23547b242012497282910300f6e6c1d5a27df82
-=======
-print("Hello once again")
->>>>>>> d67b3c03b79dac9d7aff6075d461aab24bdfe673
-=======
-print("Hello world!")
->>>>>>> 310bb8805c172bebfdecd2c8065d9c12057c518d
